b2774095f0f6a71a934f52bbfb1bb655b6a61df5
Paul Nodet <paul.nodet@gmail.com>
2021-09-09 13:37:56 +0200

    Add gitignore

--- utils/git-config.sh
+++ utils/git-config.sh
@@ -1,38 +1,32 @@
 #!/bin/sh
 rm ~/.gitconfig
 ln -sf ~/.dotfiles/git-config/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/tilde/.gitignore ~/.gitignore
 
 ## Github
 # TODO: setup ssh : use this from https://gist.github.com/kevinelliott/7152e00d6567e223902a4775b5a0a0be
 
+echo 'Checking for SSH key, generating one if it does not exist...'
+
 # Generate new ssh-key
-# ssh-keygen -t rsa -b 4096 -C "paul.nodet@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "paul.nodet@gmail.com"
 
-# Copy public key to Github.com
-# cat ~/.ssh/id_rsa.pub
+echo 'Copy this public key to Github.com'
+cat ~/.ssh/id_rsa.pub
+open 'https://github.com/account/ssh'
 
-# Test connection
-#ssh -T git@github.com
-
-#pub=$HOME/.ssh/id_ed25519.pub
-#echo 'Checking for SSH key, generating one if it does not exist...'
-#[[ -f $pub ]] || ssh-keygen -t ed25519
-
-#echo 'Copying public key to clipboard. Paste it into your Github account...'
-#[[ -f $pub ]] && cat $pub | pbcopy
-#open 'https://github.com/account/ssh'
+echo 'Test connection'
+ssh -T git@github.com
 
 # Git global config
-#git config --global user.name "Paul Nodet"
-#git config --global user.email "paul.nodet@gmail.com"
-#git config --global github.user pnodet
+git config --global user.name "Paul Nodet"
+git config --global user.email "paul.nodet@gmail.com"
+git config --global github.user pnxdxt
 #git config --global github.token your_token_here
 
-#git config --global core.editor "code -w"
-#git config --global color.ui true
+git config --global core.excludesfile ~/.gitignore
 
 ##############################################################################################################
-
 
 # github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
@@ -41,11 +35,6 @@ ln -sf ~/.dotfiles/git-config/.gitconfig ~/.gitconfig
 # speed up git status (to run only in chromium repo)
 #git config status.showuntrackedfiles no
 #git update-index --untracked-cache
-
-# faster git server communication.
-# TODO
-# like a LOT faster. https://opensource.googleblog.com/2018/05/introducing-git-protocol-version-2.html
-#git config protocol.version 2
 
 # NICE UTILS FROM https://github.com/mhartl/git-utils
 #sudo gem install git-utils
